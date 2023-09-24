630215c018ca7d3b4c461c539c185de7b3b5aec1
CnrWilson <conor.wilson@ucdconnect.ie>
2023-09-24 17:43:34 +1000

    Added deduplication

--- gitRetriever.go
+++ gitRetriever.go
@@ -30,9 +30,10 @@ func retrieveUsers(c *gin.Context) {
 
 	// Get the array of usernames from the query
 	usernames := c.QueryArray("users")
-	users := []user{}
+	usernames = deduplicate(usernames)
 
 	// For each username...
+	users := []user{}
 	for _, username := range usernames {
 
 		// ...obtain the raw user data from GitHub...
@@ -71,4 +72,27 @@ func getUserDataFromGitHub(username string) ([]byte, error) {
 		return nil, fmt.Errorf("error reading response from GitHub: %v", err)
 	}
 	return respData, nil
+}
+
+// deduplicate returns a slice of string usernames identical to the provided slice of
+// string usernames, but with duplicates removed.
+func deduplicate(usernames []string) []string {
+	output := []string{}
+	for _, username := range usernames {
+		if !contains(output, username) {
+			output = append(output, username)
+		}
+	}
+	return output
+}
+
+// contains indicates whether or not the provided slice of string usernames contains the
+// query string username.
+func contains(usernames []string, query string) bool {
+	for _, existingString := range usernames {
+		if existingString == query {
+			return true
+		}
+	}
+	return false
 }
